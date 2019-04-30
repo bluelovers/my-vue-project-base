952e795c49807f0f8e72f25e510b036f4da5d472
bluelovers <codelovers@users.sourceforge.net>
2019-04-30 10:32:45 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import store from './store'
 import './registerServiceWorker'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { production, development } from '@/lib/const'
+import '@/plugins/vue-analytics'
 
 Vue.config.productionTip = false
 
